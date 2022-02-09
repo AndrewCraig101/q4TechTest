4f2a3c9424fd3f995136b491d8cd1f599cea8edc
Andrew Craig <56005072+AndrewCraig101@users.noreply.github.com>
2022-02-09 12:18:05 -0500

    Corrected width and wrap issue of report items, added documentation.

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ var reportsWidget = {
         containerSelector: '.reports',
         template: (
             '{{#.}}' +
-                '<article class="reports_item">' +
+                '<article class="reports_item ">' +
                     '<a href="{{cover}}" target="_blank">' +
                         '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'+
                     '</a>' +
